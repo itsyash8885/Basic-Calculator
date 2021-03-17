c8b9411a80e17497044fa23e9851e7e49c035268
Yash Mangal <ymangal03@gmail.com>
2021-03-17 09:58:38 +0530

    back working

--- main.py
+++ main.py
@@ -11,20 +11,23 @@ class Calci(QMainWindow):
         self.ui.setupUi(self)
         self.stack = [0]
         self.deciactive = False
-        self.decicounter = 1
+        self.decicounter = 0
 
         for i in range(0,10):
             getattr(self.ui,"pushButton_%d"%i).pressed.connect(lambda n=i: self.inputnum(n))
         
         self.ui.pushButton_c.pressed.connect(lambda : self.clear())
         self.ui.pushButton_deci.pressed.connect(lambda : self.decipressed())
+        self.ui.pushButton_b.pressed.connect(lambda: self.back())
     
     def inputnum(self,num):
         if self.stack[0]<999999999 and self.deciactive == False:
             self.stack[0] = self.stack[0]*10 + num
-        elif self.stack[0]<float(9999999999) and self.deciactive == True and self.decicounter<5:
-            self.stack[0] = self.stack[0] + num/(10**self.decicounter)
+        elif self.stack[0]<float(9999999999) and self.deciactive == True and self.decicounter<4:
             self.decicounter += 1
+            print("inputnum-> decicounter",self.decicounter)
+            self.stack[0] = self.stack[0] + num/(10**self.decicounter)
+            
         print("stack changed in inputnum function to ",self.stack)
         self.display()
         
@@ -41,6 +44,29 @@ class Calci(QMainWindow):
         
     def decipressed(self):
         self.deciactive = True
+
+
+    def back(self):
+        if self.deciactive == False:
+            self.stack[0] = int(self.stack[0]//10)
+        elif self.deciactive == True :
+            print("back-> decicounter before",self.decicounter)
+            self.stack[0] = self.stack[0]*(10**self.decicounter)
+            print("back-> mul ",self.stack)
+            self.stack[0] = self.stack[0]//10
+            print("back-> floordiv",self.stack)
+            
+            self.decicounter-=1
+
+            if self.decicounter>0:
+                print("back-> decicounter mid",self.decicounter)
+                self.stack[0] = self.stack[0]/(10**self.decicounter)
+                print("back-> div",self.stack)
+                print("back-> decicounter after",self.decicounter)
+            elif self.decicounter==0:
+                self.deciactive = False
+
+        self.display()
 
 if __name__=="__main__":
     app=QtWidgets.QApplication(sys.argv)
